947d82a3f8714cc0427081de411200476fdb764c
Nick Tikhonov <nt34@st-andrews.ac.uk>
2016-07-16 17:58:16 -0700

    Add colors

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ HyperLine
 
 HyperLine is a status line plugin for [HyperTerm](https://hyperterm.org/).
 
-<img width="615" alt="screen shot 2016-07-16 at 14 11 28" src="https://cloud.githubusercontent.com/assets/6755555/16897203/484bea80-4b5f-11e6-9fba-f61744c28a97.png">
+<img width="678" alt="screen shot 2016-07-16 at 17 57 18" src="https://cloud.githubusercontent.com/assets/6755555/16898127/f75b50a0-4b7e-11e6-88ad-687f4702a38e.png">
--- index.js
+++ index.js
@@ -8,6 +8,7 @@ exports.decorateTerm = (Term, { React, notify }) => {
       this._drawFrame = this._drawFrame.bind(this);
       this._resizeCanvas = this._resizeCanvas.bind(this);
       this._fetchData = this._fetchData.bind(this);
+      this._cpuAverage = this._cpuAverage.bind(this);
       this._canvas = null
 
       this._fetchData()
@@ -17,6 +18,7 @@ exports.decorateTerm = (Term, { React, notify }) => {
      * Called when a new terminal instance is loaded
      */
     _onTerminal (term) {
+      console.log("OnTerm called")
       console.log(term)
       if (this.props.onTerminal) this.props.onTerminal(term);
       this._div = term._div;
@@ -48,16 +50,42 @@ exports.decorateTerm = (Term, { React, notify }) => {
       this._canvasContext.fillStyle="#222222"
       this._canvasContext.fillRect(0,this._canvas.height - 18,this._canvas.width,18)
 
-      this._canvasContext.fillStyle="#FFFFFF"
       this._canvasContext.font = 'bold 0.8pc Monospace';
 
+      this._fetchData()
+
+      var x_start = 5
+
+      this._canvasContext.fillStyle="#00D0FF"
+      let hostName = (this._sysData.hostname)
+      let toPrint = hostName + " "
+      this._canvasContext.fillText(toPrint, x_start, this._canvas.height - 5);
+      x_start += this._canvasContext.measureText(toPrint).width
+
+      this._canvasContext.fillStyle="#FFFFFF"
       let avMem  = (this._sysData.avmem / (1024 * 1024)).toFixed(0) + "MB"
       let ttMem  = (this._sysData.ttmem / (1024 * 1024)).toFixed(0) + "MB"
-      let upTime  = (this._sysData.uptime / 3600).toFixed(0) + "HRS"
-      let hostName = (this._sysData.hostname)
+      toPrint = avMem + "/" + ttMem + " "
+      this._canvasContext.fillText(toPrint, x_start, this._canvas.height - 5);
+      x_start += this._canvasContext.measureText(toPrint).width
 
-      this._fetchData()
-      this._canvasContext.fillText(hostName + " " + avMem + "/" + ttMem + " " + upTime, 5, this._canvas.height - 5);
+      this._canvasContext.fillStyle="#FFCC00"
+      let upTime  = (this._sysData.uptime / 3600).toFixed(0) + "HRS"
+      toPrint = upTime + " "
+      this._canvasContext.fillText(toPrint, x_start, this._canvas.height - 5);
+      x_start += this._canvasContext.measureText(toPrint).width
+
+      let cpuAvg = this._cpuAverage()
+      if (cpuAvg < 50) {
+        this._canvasContext.fillStyle="#15FF00"
+      } else if (cpuAvg < 75){
+        this._canvasContext.fillStyle="#F6FF00"
+      } else {
+        this._canvasContext.fillStyle="#FF0000"
+      }
+
+      toPrint = cpuAvg + "% "
+      this._canvasContext.fillText(toPrint, x_start, this._canvas.height - 5);
 
       setTimeout(() => {this._window.requestAnimationFrame(this._drawFrame)},200)
     }
@@ -69,6 +97,39 @@ exports.decorateTerm = (Term, { React, notify }) => {
         uptime: os.uptime(),
         hostname: os.hostname()
       }
+    }
+
+    _cpuAverage() {
+      console.log("Cpuavg called")
+      var totalIdle = 0, totalTick = 0;
+      var cpus = os.cpus();
+
+      for(var i = 0, len = cpus.length; i < len; i++) {
+        var cpu = cpus[i];
+
+        var type
+        for(type in cpu.times) {
+          totalTick += cpu.times[type];
+        }
+        totalIdle += cpu.times.idle;
+      }
+
+      var idle = totalIdle / cpus.length;
+      var total = totalTick / cpus.length;
+
+      var rtn = ""
+      if (this._idleCpu) {
+        var idleDifference = idle - this._idleCpu;
+        var totalDifference = total - this._totalCpu;
+        var rtn = 100 - ~~(100 * idleDifference / totalDifference);
+      } else {
+        rtn = 0
+      }
+
+      this._idleCpu = idle
+      this._totalCpu = total
+
+      return rtn
     }
 
     _resizeCanvas() {

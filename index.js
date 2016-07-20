cfdd9760c1ab9516b997547b084c405c5ce45fb4
Nick Tikhonov <nt34@st-andrews.ac.uk>
2016-07-19 20:17:39 -0700

    Light refactoring

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ exports.decorateTerm = (Term, { React, notify }) => {
       this._onTerminal = this._onTerminal.bind(this)
       this._drawFrame = this._drawFrame.bind(this)
       this._resizeCanvas = this._resizeCanvas.bind(this)
-      this._fixCanvasScaling = this._fixCanvasScaling.bind(this)
+      this._scaleCanvas = this._scaleCanvas.bind(this)
       this._fetchData = this._fetchData.bind(this)
       this._cpuAverage = this._cpuAverage.bind(this)
       this._canvas = null
@@ -26,8 +26,7 @@ exports.decorateTerm = (Term, { React, notify }) => {
     }
 
     /**
-     * Called by _onTerminal to initialize the canvas.
-     * The canvas is rendered on top of the terminal window
+     * Creates global canvas and context objects
      */
     _initCanvas () {
       this._canvas = document.createElement('canvas')
@@ -42,22 +41,28 @@ exports.decorateTerm = (Term, { React, notify }) => {
       this._window.requestAnimationFrame(this._drawFrame)
       this._window.addEventListener('resize', this._resizeCanvas)
 
-      this._fixCanvasScaling()
+      this._scaleCanvas()
     }
 
-    _fixCanvasScaling() {
-      var canvas = this._canvas,
-          context = this._canvasContext
+    /**
+     * Adjusts canvas size to account for high-dpi monitors.
+     * Ideally this should be run every time the terminal is moved
+     * to a different screen.
+     */
+    _scaleCanvas() {
+      const canvas = this._canvas
+      const ctx = this._canvasContext
 
-      var devicePixelRatio = window.devicePixelRatio || 1
-      var backingStoreRatio =
-        context.webkitBackingStorePixelRatio || context.backingStorePixelRatio || 1
+      const devicePixelRatio = window.devicePixelRatio || 1
+      const backingStoreRatio = ctx.webkitBackingStorePixelRatio
+        || ctx.backingStorePixelRatio
+        || 1
 
       this._ratio = devicePixelRatio / backingStoreRatio
 
       if (this._ratio !== 1) {
-        var oldWidth = canvas.width
-        var oldHeight = canvas.height
+        const oldWidth = canvas.width
+        const oldHeight = canvas.height
 
         canvas.width = oldWidth * this._ratio
         canvas.height = oldHeight * this._ratio
@@ -65,54 +70,78 @@ exports.decorateTerm = (Term, { React, notify }) => {
         canvas.style.width = oldWidth + 'px'
         canvas.style.height = oldHeight + 'px'
 
-        context.scale(this._ratio, this._ratio)
+        ctx.scale(this._ratio, this._ratio)
       }
     }
 
+    /**
+     * Main render function - draws the status bar on the canvas.
+     */
     _drawFrame() {
-      var ctx = this._canvasContext
+      // Color constants
+      const LINE_HEIGHT = 18
+      const LINE_PADDING = 5
+      const FONT_STYLE = "bold 0.8pc Monospace"
+      const LINE_COLOR = "#222222"
+      const HOSTNAME_COLOR = "#00D0FF"
+      const MEMORY_INFO_COLOR = "#FFFFFF"
+      const UPTIME_INFO_COLOR = "#FFCC00"
+      const CPU_USAGE_COLORS = {
+        HIGH: "#FF0000",
+        MODERATE: "#F6FF00",
+        LOW: "#15FF00"
+      }
 
-      var canvasWidth = this._canvas.width / this._ratio
-      var canvasHeight = this._canvas.height / this._ratio
+      // Prepare canvas, render line rect
+      const canvas = this._canvas
+      const ratio = this._ratio
+      const ctx = this._canvasContext
 
-      var x_start = 5
-      var y_offset = canvasHeight - 5
+      const canvasWidth = canvas.width / ratio
+      const canvasHeight = canvas.height / ratio
+
+      var x_start = LINE_PADDING
+      const y_offset = canvasHeight - LINE_PADDING
 
       ctx.clearRect(0, 0, canvasWidth, canvasHeight)
 
-      ctx.fillStyle="#222222"
-      ctx.fillRect(0,canvasHeight - 18,canvasWidth, 18)
+      ctx.fillStyle=LINE_COLOR
+      ctx.fillRect(0,canvasHeight - LINE_HEIGHT,canvasWidth, LINE_HEIGHT)
 
-      ctx.font = 'bold 0.8pc Monospace'
+      ctx.font = FONT_STYLE
 
       this._fetchData()
 
-      ctx.fillStyle="#00D0FF"
+      // Hostname
+      ctx.fillStyle = HOSTNAME_COLOR
       let hostName = (this._sysData.hostname)
       let toPrint = hostName + " "
       ctx.fillText(toPrint, x_start, y_offset)
       x_start += ctx.measureText(toPrint).width
 
-      ctx.fillStyle="#FFFFFF"
+      // Memory info
+      ctx.fillStyle = MEMORY_INFO_COLOR
       let avMem  = (this._sysData.avmem / (1024 * 1024)).toFixed(0) + "MB"
       let ttMem  = (this._sysData.ttmem / (1024 * 1024)).toFixed(0) + "MB"
       toPrint = avMem + "/" + ttMem + " "
       ctx.fillText(toPrint, x_start, y_offset)
       x_start += ctx.measureText(toPrint).width
 
-      ctx.fillStyle="#FFCC00"
-      let upTime  = (this._sysData.uptime / 3600).toFixed(0) + "HRS"
+      // Uptime info
+      ctx.fillStyle = UPTIME_INFO_COLOR
+      let upTime = (this._sysData.uptime / 3600).toFixed(0) + "HRS"
       toPrint = upTime + " "
       ctx.fillText(toPrint, x_start, y_offset)
       x_start += ctx.measureText(toPrint).width
 
-      let cpuAvg = this._cpuAverage()
+      // CPU usage
+      const cpuAvg = this._cpuAverage()
       if (cpuAvg < 50) {
-        ctx.fillStyle="#15FF00"
+        ctx.fillStyle = CPU_USAGE_COLORS.LOW
       } else if (cpuAvg < 75){
-        ctx.fillStyle="#F6FF00"
+        ctx.fillStyle = CPU_USAGE_COLORS.MODERATE
       } else {
-        ctx.fillStyle="#FF0000"
+        ctx.fillStyle = CPU_USAGE_COLORS.LOW
       }
 
       toPrint = cpuAvg + "% "
@@ -135,23 +164,24 @@ exports.decorateTerm = (Term, { React, notify }) => {
       var cpus = os.cpus()
 
       for (var i = 0, len = cpus.length; i < len; i++) {
-        var cpu = cpus[i]
+        const cpu = cpus[i]
 
         var type
         for (type in cpu.times) {
           totalTick += cpu.times[type]
         }
+
         totalIdle += cpu.times.idle
       }
 
-      var idle = totalIdle / cpus.length
-      var total = totalTick / cpus.length
+      const idle = totalIdle / cpus.length
+      const total = totalTick / cpus.length
 
       var rtn = ""
       if (this._idleCpu) {
-        var idleDifference = idle - this._idleCpu
-        var totalDifference = total - this._totalCpu
-        var rtn = 100 - ~~(100 * idleDifference / totalDifference)
+        const idleDifference = idle - this._idleCpu
+        const totalDifference = total - this._totalCpu
+        rtn = 100 - ~~(100 * idleDifference / totalDifference)
       } else {
         rtn = 0
       }
@@ -165,7 +195,7 @@ exports.decorateTerm = (Term, { React, notify }) => {
     _resizeCanvas() {
       this._canvas.width = window.innerWidth
       this._canvas.height = window.innerHeight
-      this._fixCanvasScaling()
+      this._scaleCanvas()
     }
 
     render () {

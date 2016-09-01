d921db2085cb2d6a0a4e11686fd42b78ca73fbf7
Nick Tikhonov <nt34@st-andrews.ac.uk>
2016-09-01 12:45:53 +0300

    Add back upload/download icons

--- src/lib/plugins/network.js
+++ src/lib/plugins/network.js
@@ -1,4 +1,19 @@
 import { networkStats } from 'systeminformation'
+import { iconStyles } from '../utils/icons'
+
+
+const pluginIcon = (React, fillColor) => (
+  <svg style={iconStyles} width="16px" height="16px" xmlns="http://www.w3.org/2000/svg">
+    <g fill="none" fillRule="evenodd">
+      <g fill={fillColor} transform="translate(1.000000, 1.000000)">
+        <g>
+          <path d="M0,10 L7,10 L7,11 L0,11 L0,10 Z M1,11 L6,11 L6,12 L1,12 L1,11 Z M2,12 L5,12 L5,13 L2,13 L2,12 Z M3,13 L4,13 L4,14 L3,14 L3,13 Z M2,3 L5,3 L5,10 L2,10 L2,3 Z"></path>
+          <path d="M8,2 L13,2 L13,3 L8,3 L8,2 Z M9,1 L12,1 L12,2 L9,2 L9,1 Z M10,0 L11,0 L11,1 L10,1 L10,0 Z M7,3 L14,3 L14,4 L7,4 L7,3 Z M9,4 L12,4 L12,11 L9,11 L9,4 Z"></path>
+        </g>
+      </g>
+    </g>
+  </svg>
+)
 
 export function networkSpeedFactory(React) {
   return class extends React.Component {
@@ -38,7 +53,7 @@ export function networkSpeedFactory(React) {
     render() {
       return (
         <div style={this.props.style}>
-          {this.state.download}kB/s {this.state.upload}kB/s
+          {pluginIcon(React, this.props.style.color)} {this.state.download}kB/s {this.state.upload}kB/s
         </div>
       )
     }

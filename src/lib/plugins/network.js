bae90ed46f5b19a7c95d2f0cdfe07835e2c0e0b5
Corbin Matschull <c.matschull@icloud.com>
2017-07-03 07:46:22 -0600

    Refactor overhaul to satisfy XO errors

--- src/lib/plugins/network.js
+++ src/lib/plugins/network.js
@@ -48,7 +48,7 @@ export default class Network extends Component {
 
   componentDidMount() {
     this.getSpeed()
-    this.interval = setInterval(() => this.getSpeed(), 500)
+    this.interval = setInterval(() => this.getSpeed(), 1500)
   }
 
   componentWillUnmount() {

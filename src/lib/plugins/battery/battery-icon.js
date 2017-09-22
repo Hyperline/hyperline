0376e25453b84ef2ac91e86a106017f3c2991167
Alexander Wunschik <alex@wunschik.net>
2017-09-22 09:43:15 +0200

    updated ProtoTypes import
    with this hyperline works in the current v2 version of hyper
    fixes #120

--- src/lib/plugins/battery/battery-icon.js
+++ src/lib/plugins/battery/battery-icon.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types';
 import Critical from './critical'
 import Charging from './charging'
 import Draining from './draining'

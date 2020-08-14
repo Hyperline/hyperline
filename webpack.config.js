4568f5bfd62b6cb1f7e3c41614769d3247d35dd5
Emmanuel N Kyeyune <emmakyeyune@gmail.com>
2020-08-14 19:26:07 +0800

    Updates dependencies to work with latest Hyper
    - webpack updated to version 4 and webpack.config.js modified accordingly
    - babelrc also updated to use latest loaders and syntax
    - other dependencies updated to fix security issues (npm audited)

--- webpack.config.js
+++ webpack.config.js
@@ -2,25 +2,28 @@ const webpack = require('webpack')
 const nodeExternals = require('webpack-node-externals')
 
 module.exports = {
+  mode: 'production',
   target: 'node',
   entry: './src/index.js',
   output: {
-    path: './dist',
+    path: __dirname + "/dist",
     filename: 'hyperline.js',
     libraryTarget: 'commonjs'
   },
+  devtool: 'cheap-module-source-map',
   plugins: [ new webpack.DefinePlugin({ 'global.GENTLY': false }) ],
   externals: [ nodeExternals(), 'hyper/component', 'hyper/notify', 'hyper/decorate', 'react' ],
   module: {
     rules: [
       {
-        test: /\.json$/,
-        loader: 'json-loader'
-      },
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
       }
     ]
   }
